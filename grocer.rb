e313c52e22a657e064951df404acaaf5d58ca28b
James Giosmas <giosmasj@gmail.com>
2020-03-17 04:07:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   counter = 0
   while counter < collection.length
-  binding.pry if !collection[counter]
+ # binding.pry if !collection[counter]
     if collection[counter][:item] == name
       return collection[counter]
     end
